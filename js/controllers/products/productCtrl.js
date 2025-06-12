f49565d7fe4052f2af32f8e609b8705798043f47
igorcarmona-git <igorcarmonawork@gmail.com>
2025-06-12 17:02:44 -0300

    fix: fix running command to start the app and readme docs

--- js/controllers/products/productCtrl.js
+++ js/controllers/products/productCtrl.js
@@ -1,22 +1,56 @@
+const BASEURL_API = `http://localhost:8080`;
+
 app.controller("ProductCtrl", function ($scope, $http) {
-  const BASEURL = "http://localhost:3000";
   $scope.products = [];
+  $scope.filteredProducts = [];
   $scope.filterName = "";
   $scope.categoryFilter = "";
   $scope.currentPage = 1;
   $scope.itemsPerPage = 4;
 
-  $http.get(`${BASEURL}/produtos`).then(function (response) {
-    $scope.products = response.data;
-    $scope.categories = [...new Set($scope.products.map((p) => p.categoria))];
-    $scope.totalPages = Math.ceil($scope.products.length / $scope.itemsPerPage);
+  $scope.formatPrice = function (price) {
+    const priceNumber = parseFloat(price);
+
+    if (isNaN(priceNumber)) {
+      return "Preço inválido";
+    }
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    }).format(priceNumber);
+  };
+
+  function loadProducts() {
+    $http.get(`${BASEURL_API}/produtos`).then(function (response) {
+      if (!response.data) return "Nenhum produto encontrado";
+      $scope.products = response.data;
+      $scope.categories = [...new Set($scope.products.map((p) => p.categoria))];
+
+      $scope.filterProducts();
+    });
+  }
+
+  $scope.filterProducts = function () {
+    $scope.filteredProducts = $scope.products.filter((product) => {
+      const matchesName = product.nome
+        .toLowerCase()
+        .includes($scope.filterName.toLowerCase());
+      const matchesCategory =
+        !$scope.categoryFilter || product.categoria === $scope.categoryFilter;
+      return matchesName && matchesCategory;
+    });
+
+    $scope.totalPages = Math.ceil(
+      $scope.filteredProducts.length / $scope.itemsPerPage
+    );
     $scope.pagination();
-  });
+  };
 
   $scope.pagination = function () {
     const start = ($scope.currentPage - 1) * $scope.itemsPerPage;
     const end = start + $scope.itemsPerPage;
-    $scope.paginatedProducts = $scope.products.slice(start, end);
+    $scope.paginatedProducts = $scope.filteredProducts.slice(start, end);
   };
 
   $scope.changePage = function (direction) {
@@ -27,4 +61,6 @@ app.controller("ProductCtrl", function ($scope, $http) {
     }
     $scope.pagination();
   };
+
+  loadProducts();
 });
